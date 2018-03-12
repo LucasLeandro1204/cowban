8534263c7944119fb46d639fffb1a6c160791c54
DoctorVWA <doctorvwa43@gmail.com>
2018-03-12 16:12:47 -0300

    fixes3

--- src/core/jwt.js
+++ src/core/jwt.js
@@ -1,6 +1,6 @@
 import Config from 'config';
-import JWT from 'jsonwebtoken';
 import User from 'model/user';
+import JWT from 'jsonwebtoken';
 
 export default (user, lifetime) => {
   return JWT.sign(
