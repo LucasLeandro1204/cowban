c2fb2dd3328b36abaf605a4926a355f357652f01
doctorvwa <vi4-weigert@hotmail.com>
2018-03-12 14:46:02 -0300

    token thing

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -1,12 +1,19 @@
 import Config from 'config';
 import JWT from 'express-jwt';
+import jwt from 'core/jwt';
 
-export default JWT({
-  secret: Config.get('jwt.secret'),
-  requestProperty: 'auth',
-  getToken (req) {
-    const splited = req.headers.authorization ? req.headers.authorization.split(' ') : [];
+export default [
+	JWT({
+  	secret: Config.get('jwt.secret'),
+  	getToken (req) {
+   		const splited = req.headers.authorization ? req.headers.authorization.split(' ') : [];
 
-    return splited[0] === 'Bearer' ? splited[1] : null;
+    	return splited[0] === 'Bearer' ? splited[1] : null;
+  	},
+	}),
+	(req, res) => {
+		const token = jwt(req.user);
+
+    res.send({ token });
   },
-});
+];
