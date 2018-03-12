5568cf072a3babf1b0ab06b887ef0cdcc22a963d
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-12 04:17:06 -0300

    yeap

--- src/core/jwt.js
+++ src/core/jwt.js
@@ -3,12 +3,6 @@ import User from 'model/user';
 import JWT from 'jsonwebtoken';
 
 export default (user, lifetime) => {
-
-  // useless?
-  if (! (user instanceof User)) {
-    throw new Error('Only users can be authenticated!');
-  }
-
   return JWT.sign(
     {
       id: user.id,
