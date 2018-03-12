7628234cb6554609a165e3215e29733fd10c24c8
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-12 03:50:48 -0300

    update helper

--- src/support/helpers.js
+++ src/support/helpers.js
@@ -9,24 +9,22 @@ const rollback = (done = () => {}) => (
 
 const wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
 
-const schema = obj => ({
-  status: status => [
-    checkSchema(obj),
-    (req, res, next) => {
-      const errors = validationResult(req);
+const schema = (obj, status = 422) => ([
+  checkSchema(obj),
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-      if (! errors.isEmpty()) {
-        return res.status(status).json({
-          errors: errors.mapped(),
-        });
-      }
-
-      set(req, 'validated', matchedData(req));
-
-      next();
+    if (! errors.isEmpty()) {
+      return res.status(status).json({
+        errors: errors.mapped(),
+      });
     }
-  ],
-});
+
+    set(req, 'validated', matchedData(req));
+
+    next();
+  }
+]);
 
 export {
   wrap,
