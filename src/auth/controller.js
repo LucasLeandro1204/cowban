d3aaf75b2521b874d0f4bf08704791f20901de2b
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-12 01:19:57 -0300

    ping

--- src/auth/controller.js
+++ src/auth/controller.js
@@ -1,7 +1,27 @@
 import { wrap } from 'support/helpers';
 import Authenticated from 'auth/middleware';
+import{ checkSchema } from 'express-validator/check';
 
 class AuthController {
+  static login () {
+    return [
+      checkSchema({
+
+      }),
+      wrap(async (req, res) => {
+
+      }),
+    ];
+  }
+
+  static ping () {
+    return [
+      Authenticated,
+      (req, res) => {
+        res.send('Pong!');
+      },
+    ];
+  }
 };
 
 export default AuthController;
--- src/auth/index.js
+++ src/auth/index.js
@@ -1,6 +1,9 @@
 import JWT from 'core/jwt';
 import { Router } from 'express';
+import Controller from 'auth/controller';
 
 const router = Router();
+
+router.get('/ping', Controller.ping());
 
 export default router;
