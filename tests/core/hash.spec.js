b9a01136a5bc3b42f48b11b6f158cc2bd1298fe8
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-12 23:56:41 -0300

    hash test

---
+++ tests/core/hash.spec.js
@@ -0,0 +1,32 @@
+import Config from 'config';
+import Hash from 'core/hash';
+import { expect } from 'support/chai';
+
+describe('Hash @hash', () => {
+  it('rounds should be the same as in config', async () => {
+    expect(Hash._rounds).to.be.equals(Config.get('hash.rounds'));
+  });
+
+  it('should encrypt the content', async () => {
+    const string = 'Foo';
+    const encrypted = await Hash.encrypt(string);
+
+    expect(encrypted).to.not.be.equals(string);
+  });
+
+  it('should return false if content does not match', async () => {
+    const string = 'Foo';
+    const encrypted = await Hash.encrypt('Random');
+    const compare = await Hash.compare(string, encrypted);
+
+    expect(compare).to.be.false;
+  });
+
+  it('should return true if content does match', async () => {
+    const string = 'Foo';
+    const encrypted = await Hash.encrypt(string);
+    const compare = await Hash.compare(string, encrypted);
+
+    expect(compare).to.be.true;
+  });
+});
