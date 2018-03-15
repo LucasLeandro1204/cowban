cefea4908c6ad6141dd7596973090b563da500fa
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-14 22:05:34 -0300

    drop

--- src/support/helpers.js
+++ src/support/helpers.js
@@ -1,7 +1,4 @@
 import Knex from 'core/knex';
-import { set } from 'lodash';
-import { matchedData } from 'express-validator/filter';
-import { checkSchema, validationResult } from 'express-validator/check';
 
 const rollback = (done = () => {}) => (
   Knex.seed.run() // because seed already drops the data before run xd
@@ -9,25 +6,7 @@ const rollback = (done = () => {}) => (
 
 const wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
 
-const schema = (obj, status = 422) => ([
-  checkSchema(obj),
-  (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (! errors.isEmpty()) {
-      return res.status(status).json({
-        errors: errors.mapped(),
-      });
-    }
-
-    set(req, 'validated', matchedData(req));
-
-    next();
-  }
-]);
-
 export {
   wrap,
-  schema,
   rollback,
 };
