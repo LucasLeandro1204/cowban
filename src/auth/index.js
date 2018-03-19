bef26417b36188f0411f98b4aeb6511ee0560b33
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-19 19:56:51 -0300

    use router

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,25 +1,25 @@
+import Router from 'core/router';
 import Login from 'auth/job/login';
 import Register from 'auth/job/register';
-import Controller from 'core/controller';
 import Authenticated from 'auth/middleware';
 import LoginValidation from 'auth/validate/login';
 import RegisterValidation from 'auth/validate/register';
 import { AuthenticationError, EmailAlreadyTaken } from 'auth/errors';
 
-const controller = new Controller();
+const router = new Router();
 
-controller
-  .get('/ping', (req, res) => res.send('Pong!'))
-    .before(Authenticated)
-  .post('/login', Login)
-    .validate(LoginValidation)
-    .after((err, req, res, next) => {
-      throw new AuthenticationError;
-    })
-  .post('/register', Register)
-    .validate(RegisterValidation)
-    .after((err, req, res, next) => {
-      throw new EmailAlreadyTaken;
-    });
-
-export default controller.setup();
+export default router
+  .prefix('/auth')
+    .get('/ping', (req, res) => res.send('Pong!'))
+      .before(Authenticated)
+    .post('/login', Login)
+      .validate(LoginValidation)
+      .handle(() => {
+        throw new AuthenticationError;
+      })
+    .post('/register', Register)
+      .validate(RegisterValidation)
+      .handle(() => {
+        throw new EmailAlreadyTaken;
+      })
+  .build();
